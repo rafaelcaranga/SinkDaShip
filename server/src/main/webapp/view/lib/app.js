eb1170169e3a3811226069c91314bf1af66ba0ad
Rafael Moreira <rafaelrmoreira@gmail.com>
2016-10-25 16:13:53 -0300

    Integration with new player method in back-end, missing to redirect to match form

--- server/src/main/webapp/view/lib/app.js
+++ server/src/main/webapp/view/lib/app.js
@@ -8,6 +8,12 @@ app.config(['uiMask.ConfigProvider', function(uiMaskConfigProvider) {
 
 app.controller('sinkController', function ($scope, $http) {
 
+	var dictLetters = {
+							"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,"J":10,
+							"a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8,"i":9,"j":10
+						};
+	var dictNumbers = {"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"0":10};
+
 	$scope.player = {
 		"board" : {
 			"ships" : [
@@ -89,11 +95,35 @@ app.controller('sinkController', function ($scope, $http) {
 
 	$scope.enterNewGame = function (player) {
 		console.log('enterNewGame');
-		$http.post('http://rnlabs.com.br:8080/sinkdaship/match/add-new-player',JSON.stringify(player)).then(enterNewGameSuccessCallback, enterNewGameErrorCallback);
+		if ($scope.form.$valid){
+			var playerParameters = preparePlayerParameters(player);
+			$http.post('http://rnlabs.com.br:8080/sinkdaship/match/add-new-player',playerParameters).then(enterNewGameSuccessCallback, enterNewGameErrorCallback);	
+		}
 	};
 
-	var enterNewGameSuccessCallback = function (data) {
-		console.log('success while entering new game',data);
+	var preparePlayerParameters = function (player) {
+		for (var shipIndex = 0; shipIndex < player.board.ships.length; shipIndex++) {
+			var currentShip = player.board.ships[shipIndex];
+			for (var partIndex = 0; partIndex < currentShip.parts.length; partIndex++) {
+				var positions = currentShip.parts[partIndex].position.split("",2);
+				
+				currentShip.parts[partIndex].positionOnTable = {
+					"verticalPosition": dictLetters[positions[0]],
+					"horizontalPosition": dictNumbers[positions[1]]
+				};
+			};
+		};
+		return JSON.stringify(player);
+	}
+
+	var enterNewGameSuccessCallback = function (response) {
+		var data = response.data;
+		console.log(data);
+		if (data.ok){
+			console.log('success entering in new match, take your ID: '+data.resultObject.matchIdentificator+' you are the player '+data.resultObject.playerNumber);
+		} else {
+			enterNewGameErrorCallback(data);
+		}
 	};
 
 	var enterNewGameErrorCallback = function (data) {
